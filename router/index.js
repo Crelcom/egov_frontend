5e4d7b5cb78a6b73f003e3236f9e5fa94c6ab71c
Aleksandr Nazarchuk <nitrogen2009@mail.ru>
2014-07-29 14:04:11 +0400

    Helper function for needed headers added. Error on /admin (not /admin/*) path fixed

--- router/index.js
+++ router/index.js
@@ -18,6 +18,23 @@ function checkCookies(req, callback){
     callback(null, true);
 }
 
+// helper function to get needed headers
+function getHeaders(request) {
+    var headers = {
+        'Connection': request.headers.connection,
+        'Host': request.headers.host,
+        'User-Agent': request.headers['user-agent'],
+        'Content-Type': 'application/json'
+    }
+    if (!_.isEmpty(request.headers.cookie)) {
+        headers['Cookie'] = request.headers.cookie;
+    }
+    if (!_.isEmpty(request.headers['x-csrf-token'])) {
+        headers['X-CSRF-Token'] = request.headers['x-csrf-token'];
+    }
+    return headers;
+}
+
 module.exports = function(express){
     // router instance
     var router = express.Router();
@@ -36,15 +53,27 @@ module.exports = function(express){
     // universal module loader
     router.route('/:module')
         .get(function(req, res, next) {
-            checkCookies(req, function(err, data){
-                var module = req.params.module;
-                if(err && module !== 'login'){
-                    res.redirect('/login');
-                }else{
-                    // @todo check if module exist
-                    res.render(module);
-                }
-            });
+            var module = req.params.module;
+            var method = methods[req.method];
+
+            if (module === 'admin') {
+                var url = config.get('backend')+req.url;
+                proxy[method]({url: url, headers: getHeaders(req)}, function(err, httpResponse, body) {
+                    if (err) {
+                        next(err);
+                    }
+                    res.send(body);
+                });
+            } else {
+                checkCookies(req, function (err, data) {
+                    if (err && module !== 'login') {
+                        res.redirect('/login');
+                    } else {
+                        // @todo check if module exist
+                        res.render(module);
+                    }
+                });
+            }
         });
 
 
@@ -57,14 +86,7 @@ module.exports = function(express){
             var path = req.url,
                 url = config.get('backend') + path,
                 params = {},
-                options = {
-                    url: url,
-                    headers: {
-                        'Cookie': req.headers.cookie,
-                        'Content-Type': 'application/json'
-                    }
-//                    headers: req.headers
-                },
+                options = {url: url, headers: getHeaders(req)},
                 meth = methods[req.method];
 
             if(req.body && !_.isEmpty(req.body)){
@@ -85,7 +107,7 @@ module.exports = function(express){
                 addUrl = config.get('backend') + path,
                 isAdminUrl = config.get('backend') + '/api/is_admin',
                 params = {},
-                headers = {'Cookie': req.headers.cookie, 'Content-Type': 'application/json'},
+                headers = getHeaders(req),
                 method = methods[req.method],
                 is_admin = 0;
 
@@ -116,14 +138,7 @@ module.exports = function(express){
             var path = req.url,
                 url = config.get('backend') + path,
                 params = {},
-                options = {
-                    url: url,
-                    headers: {
-                        'Cookie': req.headers.cookie,
-                        'Content-Type': 'application/json'
-                    }
-    //                    headers: req.headers
-                },
+                options = {url: url, headers: getHeaders(req)},
                 meth = methods[req.method];
 
             if(req.body && !_.isEmpty(req.body)){
